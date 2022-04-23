f3cbc00c26acb70d0fa86b374e01f0d6fc492840
Adail Spinola Horst <the.spaww@gmail.com>
2022-04-22 22:01:36 -0300

    Implements task Guia estelar de JavaScripts - capitulo 5

--- Guia Estelar de JavaScript/scripts.js
+++ Guia Estelar de JavaScript/scripts.js
@@ -1,1 +1,45 @@
-console.log('Bem vindos ao starter!');
+console.log("Bem vindos ao starter!");
+let weight;
+console.log("1", "weight type", typeof weight);
+let student = {
+  name: "João",
+  age: 20,
+  stars: 4.8,
+  weight: 45,
+  isSubscribed: true,
+};
+console.log("2", "student type", typeof student);
+
+let students = {};
+
+console.log("3", "students type", typeof students);
+
+console.log(
+  "4",
+  `${student.name} tem ${student.age} anos e pesa ${student.weight} kg`
+);
+
+students = [];
+
+console.log("5", "students type", typeof students);
+
+students = [student];
+
+console.log("6", "students", students);
+
+console.log("7", "students[0]", students[0]);
+
+students[1] = {
+  name: "Maria",
+  age: 24,
+  stars: 5.0,
+  weight: 35,
+  isSubscribed: true,
+};
+console.log("8", "students[1]", students[1]);
+
+
+console.log("9", "resultado de analise do codigo:", "undefined");
+
+console.log(a);
+var a = 1;
